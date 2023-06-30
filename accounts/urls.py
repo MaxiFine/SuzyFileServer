af323db596e56035facbe02d564b59a2f28ad6f0
Edward Hanson <gibboel5@gmail.com>
2023-06-30 16:01:13 +0000

    refactor urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import signup_view
+from .views import signup_view, activate, login_view
 
 urlpatterns = [
     path('signup/', signup_view, name='signup'),
+    path('activate/<uidb64>/<token>/', activate, name='activate'),
+    path('login/', login_view, name='login'),
     
 ]
