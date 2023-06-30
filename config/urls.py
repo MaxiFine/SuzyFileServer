e57db518b2a7f2bc338f85c043e1f3140b90a544
Edward Hanson <gibboel5@gmail.com>
2023-06-30 16:05:39 +0000

    Refactoring urls

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include("accounts.urls")),
     path('accounts/', include('django.contrib.auth.urls')), 
-    path('signup/', include("accounts.urls")),
     path('', include('pages.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
