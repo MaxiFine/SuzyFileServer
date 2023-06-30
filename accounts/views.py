473a4fdf4fe12eec52cc3cd45ff47c17a2375e5e
Edward Hanson <gibboel5@gmail.com>
2023-06-30 16:03:56 +0000

    add views for signup, login, account activation

--- accounts/views.py
+++ accounts/views.py
@@ -1,39 +1,88 @@
-# # CBV imports
-# from django.contrib.auth.forms import UserCreationForm
-# from django.urls import reverse_lazy
-# from django.views.generic import CreateView
-
-
-# FBV imports
 from django.contrib.auth import get_user_model
 from django.shortcuts import render, redirect
-from .forms import SignUpForm
+from django.urls import reverse
+from django.contrib.auth import login, authenticate
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.contrib.auth.tokens import default_token_generator
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.contrib import messages
+
+from .forms import SignUpForm, LoginForm
 
 
-
-# # # Class Based View to test the signup
-# # class SignUpView(CreateView):
-# #     form_class = UserCreationForm
-# #     success_url = reverse_lazy("login")
-# #     template_name = "registration/signup.html"
-
-
-
-# FBV for user signups
 def signup_view(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            user.username = user.email
+            user.is_active = False
             user.save()
-            return redirect('login')  # Redirect to the login page
+            
+            # let's email to the user for account verification
+            host_name = request.get_host()
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            token = default_token_generator.make_token(user)
+            activation_link = f"{request.scheme}://{host_name}{reverse('activate', kwargs={'uidb64': uid, 'token': token})}"
+            email_html_message = render_to_string('registration/account_activation_email.html', {
+                'user': user,
+                'activation_link':activation_link,
+            })
+            
+            email = EmailMessage(
+                'Account Activation',
+                email_html_message,
+                from_email='Suzzy',
+                to=[user.email],
+            )
+            
+            email.content_subtype="html"
+            email.send()
+            messages.info(request=request, message='Account Created. A link has been sent to your Email')
+            print(user.email)
+            return redirect('login')
     else:
         form = SignUpForm()
-        # Restrict the UserCreationForm fields to only email and password
-        # form.fields.pop('username')
     return render(request, 'registration/signup.html', {'form': form})
 
 
 
+def activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = get_user_model().objects.get(pk=uid)
+    except (TypeError, ValueError, get_user_model().DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request=request, message='Account Activated. Kindly Login')
+        return redirect('login')
+    else:
+        messages.error(request, 'Invalid Activation Link')
+        return redirect('login')
     
+def login_view(request):
+    # prevent authenticated user from accessing this view
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('home')
+                messages.warning(request, 'Account Not Activated. Check Your Email/Spam for Activation link!')
+                return redirect('login')
+            messages.error(request, 'Wrong Email or Password!')
+            return redirect('login')
+    else:
+        form = LoginForm()
+    return render(request, 'registration/login.html', {'form': form})
