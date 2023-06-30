7c63007a247dd5199230e28c598f3f0403ebc8bf
Edward Hanson <gibboel5@gmail.com>
2023-06-30 16:05:08 +0000

    Email Configurations

--- config/settings.py
+++ config/settings.py
@@ -150,7 +150,10 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'about'
 
 # email setups
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend" 
-
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT =  587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'gibboel5@gmail.com'
+EMAIL_HOST_PASSWORD = 'jykgweldirbyeekb'
  
